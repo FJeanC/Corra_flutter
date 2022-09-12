da2c0ebca992b6a9aac3fa1dcf93802d62ab894d
FJeanC <jeanfagiani1@gmail.com>
2022-09-12 15:40:39 -0300

    Deleta imagem junto com a corrida do firebase

--- lib/services/cloud/firebase_cloud_run_storage.dart
+++ lib/services/cloud/firebase_cloud_run_storage.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:corra/services/cloud/cloud_run.dart';
 import 'package:corra/services/cloud/cloud_storage_constants.dart';
 import 'package:corra/services/cloud/cloud_storage_exceptions.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseCloudRunStorage {
   final runs = FirebaseFirestore.instance.collection('runs');
@@ -30,6 +31,14 @@ class FirebaseCloudRunStorage {
       await runs.doc(documentId).delete();
     } catch (e) {
       throw CouldNotDeleteRunException();
+    }
+    try {
+      final ref =
+          FirebaseStorage.instance.ref().child('runs_image/${documentId}');
+      await ref.delete();
+    } catch (e) {
+      print('Maybe ');
+      return;
     }
   }
 
